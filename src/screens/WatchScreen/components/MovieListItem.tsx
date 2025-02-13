835f7988a6f2318fad3a276956dc2bfaa7883c30
Dave Paiva <mrdavepaiva@gmail.com>
2025-02-13 19:39:33 +0530

    FIX movie list

--- src/screens/WatchScreen/components/MovieListItem.tsx
+++ src/screens/WatchScreen/components/MovieListItem.tsx
@@ -2,18 +2,19 @@ import { renderTMDBImage } from '@app_utils/helperfuncs';
 import React from 'react';
 import { 
   ImageBackground,
-  Text,
   StyleSheet,
   View,
   Pressable,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-
+import Text from '@components/Text';
 interface MovieListItemProps {
   title: string;
   posterUrl: string;
   onPress: () => void;
 }
+
+const BORDER_RADIUS = 20;
 
 const MovieListItem: React.FC<MovieListItemProps> = ({ title, posterUrl, onPress }) => {
 
@@ -23,13 +24,14 @@ const MovieListItem: React.FC<MovieListItemProps> = ({ title, posterUrl, onPress
         source={{ uri: renderTMDBImage(posterUrl, 500) }}
         style={styles.imageBackground}
         resizeMode="cover"
+        imageStyle={styles.image}
       >
         <LinearGradient
           colors={['transparent', '#000000']}
           style={styles.gradient}
         >
           <View style={styles.contentContainer}>
-            <Text style={styles.title} numberOfLines={2}>
+            <Text variant="light" weight="Medium" size="x_large" numberOfLines={2}>
               {title}
             </Text>
           </View>
@@ -42,28 +44,22 @@ const MovieListItem: React.FC<MovieListItemProps> = ({ title, posterUrl, onPress
 const styles = StyleSheet.create({
   card: {
     width: '100%',
-    height: 180,
-    borderRadius: 20,
     marginBottom: 20,
   },
   imageBackground: {
     width: '100%',
-    height: '100%',
+    height: 180,
+  },
+  image: {
+    borderRadius: 20,
   },
   gradient: {
     flex: 1,
     justifyContent: 'flex-end',
+    borderRadius: BORDER_RADIUS,
   },
   contentContainer: {
     padding: 16,
-  },
-  title: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: 'bold',
-    textShadowColor: 'rgba(0, 0, 0, 0.75)',
-    textShadowOffset: { width: 0, height: 1 },
-    textShadowRadius: 3,
   },
 });
 
